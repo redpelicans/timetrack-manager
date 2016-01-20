9bf5a2bf8bdf0d201d9fae612f10e7d0e838c218
root <root@rp3>
2016-01-20 15:09:50 +0100

    fix stream

--- timetrack-docker/start.sh
+++ timetrack-docker/start.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-PRG="/timetrack"
-CONFIG="/config"
-
-PARAMS="params.js"
-
-cd "$PRG"
-
-ln -fs "$CONFIG/$PARAMS" "$PRG/$PARAMS"
-
-echo "Running timetrack  ..."
-NODE_ENV=production DEBUG=timetrack:* node server/lib/main.js
+NODE_ENV=production DEBUG=timetrack:* node server/dist/main.js
